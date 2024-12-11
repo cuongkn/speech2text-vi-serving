4cf64063fbcdfd1e4fbce0843cbe1f06f9f91066
cuongkn <knc.uetvnu@gmail.com>
2024-12-12 00:58:45 +0700

    update

--- triton/whisper/lauch_server.sh
+++ triton/whisper/lauch_server.sh
@@ -2,8 +2,8 @@ export CUDA_VISIBLE_DEVICES="0"
 
 model_repo_path=/triton/whisper/model_repo_whisper_tllm
 
-tritonserver --model-repository $model_repo_path \
-            --pinned-memory-pool-byte-size=2048000000 \
-            --cuda-memory-pool-byte-size=0:4096000000 \
-            --http-port 10086 \
-            --metrics-port 10087
+tritonserver --model-repository $model_repo_path 
+            # --pinned-memory-pool-byte-size=2048000000 \
+            # --cuda-memory-pool-byte-size=0:4096000000 \
+            # --http-port 10086 \
+            # --metrics-port 10087
