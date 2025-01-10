2f493f3eaed80305884d313345b36eedde035146
cuongkn <knc.uetvnu@gmail.com>
2025-01-10 18:28:57 +0700

    update tllm version

--- tllm/whisper-tensorrt-llm/b.sh
+++ tllm/whisper-tensorrt-llm/b.sh
@@ -1,6 +1,6 @@
 model_name="$1"
 
-INFERENCE_PRECISION=float32
+INFERENCE_PRECISION=float16
 WEIGHT_ONLY_PRECISION=int8
 MAX_BEAM_WIDTH=4
 MAX_BATCH_SIZE=16
@@ -8,39 +8,34 @@ checkpoint_path=engines/
 checkpoint_dir=${checkpoint_path}${model_name}_weights
 output_dir=${checkpoint_path}${model_name}
 
-python3 tllm/whisper-tensorrt-llm/convert_checkpoint.py \
+python3 tllm/whisper-tensorrt-llm/cv_cp.py \
                 --model_name $model_name \
-                --dtype float16 \
-                --logits_dtype float32 \
                 --output_dir $checkpoint_dir
 
 # Build the large-v3 model using trtllm-build
 trtllm-build  --checkpoint_dir ${checkpoint_dir}/encoder \
               --output_dir ${output_dir}/encoder \
-              --paged_kv_cache disable \
-              --remove_input_padding disable \
-              --context_fmha disable \
               --moe_plugin disable \
-              --enable_xqa disable \
               --max_batch_size ${MAX_BATCH_SIZE} \
               --gemm_plugin disable \
               --bert_attention_plugin ${INFERENCE_PRECISION} \
-              --remove_input_padding disable \
-              --max_input_len 3000
-            #   --max_seq_len=3000
+              --max_input_len 3000 \
+              --max_seq_len=3000 \
+              --paged_kv_cache enable \
+              --remove_input_padding enable 
+              
 
 trtllm-build  --checkpoint_dir ${checkpoint_dir}/decoder \
               --output_dir ${output_dir}/decoder \
-              --paged_kv_cache disable \
-              --remove_input_padding disable \
-              --context_fmha disable \
               --moe_plugin disable \
-              --enable_xqa disable \
               --max_beam_width ${MAX_BEAM_WIDTH} \
               --max_batch_size ${MAX_BATCH_SIZE} \
+              --max_seq_len 114 \
               --max_input_len 14 \
               --max_encoder_input_len 3000 \
               --gemm_plugin ${INFERENCE_PRECISION} \
               --bert_attention_plugin ${INFERENCE_PRECISION} \
-              --gpt_attention_plugin ${INFERENCE_PRECISION}
-            #   --max_seq_len 114 \
+              --gpt_attention_plugin ${INFERENCE_PRECISION} \
+              --paged_kv_cache enable \
+              --remove_input_padding enable 
+              
---
+++ triton/whisper/prepare.sh
@@ -0,0 +1,46 @@
+launch_triton_repo_python_backend() {
+
+    local engine_dir=$1
+
+    n_mels=$(cat ${engine_dir}/encoder/config.json | grep n_mels | awk -F': ' '{print $2}' | tr -d ',')
+
+    if [[ "$engine_dir" == *"multi-hans"* ]]; then
+        zero_pad=true # fine-tuned model could remove 30s padding, so set pad to none
+    else
+        zero_pad=false
+    fi
+
+    echo "engine_dir: $engine_dir", "n_mels: $n_mels", "zero_pad: $zero_pad"
+
+    model_repository=whisper_model_repository
+    
+    rm -rf $model_repository
+    cp -r whisper_model_repository_trtllm $model_repository
+    wget -nc --directory-prefix=$model_repository/infer_bls/1 https://raw.githubusercontent.com/openai/whisper/main/whisper/assets/multilingual.tiktoken
+    wget -nc --directory-prefix=$model_repository/whisper/1 https://raw.githubusercontent.com/openai/whisper/main/whisper/assets/mel_filters.npz
+
+    TRITON_MAX_BATCH_SIZE=64
+    MAX_QUEUE_DELAY_MICROSECONDS=100
+
+    python3 fill_template.py -i $model_repository/whisper/config.pbtxt engine_dir:${engine_dir},n_mels:$n_mels,zero_pad:$zero_pad,triton_max_batch_size:${TRITON_MAX_BATCH_SIZE},max_queue_delay_microseconds:${MAX_QUEUE_DELAY_MICROSECONDS}
+    python3 fill_template.py -i $model_repository/infer_bls/config.pbtxt engine_dir:${engine_dir},triton_max_batch_size:${TRITON_MAX_BATCH_SIZE},max_queue_delay_microseconds:${MAX_QUEUE_DELAY_MICROSECONDS}
+    
+    echo "Launching triton server with model_repo: $model_repository"
+    
+    tritonserver --model-repository=$model_repository
+}
+
+
+MODEL_IDs=("whisper-medium-finetuned")
+CUDA_VISIBLE_DEVICES=0
+
+model_id=$1
+
+engine_dir="/engines/${model_id}"
+
+if printf '%s\n' "${MODEL_IDs[@]}" | grep -q "^$model_id$"; then
+    launch_triton_repo_python_backend "$engine_dir" || exit 1
+else
+    echo "$model_id is NOT in the MODEL_IDs array."
+    exit 1
+fi
